5491c392ec0791311d22ccc120caed349da3dd95
IgorLev <igorlevdansky91@gmail.com>
2025-04-14 13:33:24 +0300

    Fixed database

--- models/index.ts
+++ models/index.ts
@@ -5,14 +5,16 @@ import { scores } from './scores';
 import { config } from "../config/config"
 import { Sequelize } from 'sequelize-typescript';
 
-//const env = process.env.NODE_ENV || 'development';
-const sequelizeConfig = config['development'];
+const env = process.env.NODE_ENV || 'development';
+const sequelizeConfig = config[env];
 
 const sequelize = new Sequelize({
   database: sequelizeConfig.database,
   dialect: sequelizeConfig.dialect,
   username: sequelizeConfig.username,
   password: sequelizeConfig.password,
+  host: sequelizeConfig.host,
+  port: Number(sequelizeConfig.port),
   models: [users, scores]
 });
 
